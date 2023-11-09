47f723978419b74aa573f426aed537d8a23e31c0
NBSC-Cliff <reconcliffy@gmail.com>
2023-11-09 15:57:22 +0800

    Summoned lester

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
-
-
 public class Main {
 
     public static void main(String[] args) {
       
         Dog myDog = new Dog("Lester");
+        
+        myDog.eat();
+        myDog.sleep();
         
         
                 
